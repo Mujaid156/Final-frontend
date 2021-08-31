b9fbbf8f1a0c15fd93e8e057bd92b84a377a7fef
Mujaid156 <mujaid.kariem22@gmail.com>
2021-08-31 16:29:45 +0200

    third commit

--- javascript/login.js
+++ javascript/login.js
@@ -1,23 +1,17 @@
-fetch("https://sleepy-crag-84730.herokuapp.com/")
-.then((res) => res.json())
-.then((data) => {
-    console.log(data);
-});
-
 function  login() {
-    let email = document.querySelector('#email').value;
-    let password = document.querySelector('#password').value;
-    console.log(email, password);
+    // let email = document.querySelector('#email').value;
+    // let password = document.querySelector('#password').value;
+    // console.log(email, password);
 
-    fetch("https://sleepy-crag-84730.herokuapp.com/user-registration/", {
-        method: 'POST',
-        body: JSON.stringify({
-            email: email,
-            password: password
-        }),
+    fetch("https://sleepy-crag-84730.herokuapp.com/login", {
+        method: 'PATCH',
         headers: {
             'Content-type': 'application/json',
         },
+        body: JSON.stringify({
+            email: document.querySelector('#email').value,
+            password: document.querySelector('#password').value
+        })
     })
     .then(res => res.json())
     .then(data => {
